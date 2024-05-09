ae80483c1b38d6ab826c05f12efbda0ea6bc7b79
rweathers89 <rachelb.weathers@gmail.com>
2024-05-08 22:00:54 -0700

    add components and test files; updated package.json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,48 @@
-import React from 'react';
+// src/App.js
+
+import CitySearch from './components/CitySearch';
+import EventList from './components/EventList';
+import NumberOfEvents from './components/NumberOfEvents';
+import './App.css';
+import { useEffect, useState } from 'react';
+import { extractLocations, getEvents } from './api';
+
+
+
+const App = () => {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
+  const [currentCity, setCurrentCity] = useState("See all cities");
+  const [errorAlert, setErrorAlert] = useState('');
+
+  useEffect(() => {
+    fetchData();
+  }, [currentCity, currentNOE]);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    const filteredEvents = currentCity === "See all cities" ?
+      allEvents :
+      allEvents.filter(event => event.location === currentCity)
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  }
+
+
+  return (
+    <div className="App">
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
+      <NumberOfEvents setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert} />
+      <EventList events={events} />
+    </div>
+  );
+}
+
+export default App;
+
+/**
+ * import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -24,3 +68,4 @@ function App() {
 }
 
 export default App;
+ */
---
+++ src/components/NumberOfEvents.js
@@ -0,0 +1,39 @@
+// src/components/NumberOfEvents.js
+
+//import { useState } from 'react';
+
+const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
+    // const [eventNumber, setEventNumber] = useState();
+    const handleInputChaged = (event) => {
+        const value = event.target.value;
+        setCurrentNOE(value);
+
+        if (isNaN(value)) {
+            setErrorAlert('value is not a number');
+        } else if (value > 50) {
+            setErrorAlert('maximum value is 50');
+        } else if (value <= 0) {
+            setErrorAlert('minimum value is 1');
+        } else {
+            setErrorAlert('');
+            setCurrentNOE(value);
+        }
+    };
+
+
+    return (
+        <div id="number-of-events">
+            <input
+                type="text"
+                defaultValue={'32'}
+                className="number-of-events-input"
+                //value={eventNumber}
+
+                onChange={handleInputChaged}
+            />
+
+        </div>
+    )
+}
+
+export default NumberOfEvents;
