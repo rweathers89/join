06bda761eeb606aee0c200a71146ca3f778709b8
rweathers89 <rachelb.weathers@gmail.com>
2024-05-11 18:12:49 -0700

    add Atatus App Performance Monitoring

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('dfc91d09cfec4289ac42fca6479ec72f').install();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -21,3 +24,6 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// check that Atatus has been integrated successfully
+//atatus.notify(new Error('Test Atatus Setup'));
